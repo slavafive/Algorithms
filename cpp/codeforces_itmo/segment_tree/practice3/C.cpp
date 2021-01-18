3e01ee6a69d01fd2e8c5f254ae0fb699a1cf77fa
Vyacheslav Efimov <efimov@element-soft.com>
2021-01-18 16:41:17 +0300

    Solve 3C

--- cpp/codeforces_itmo/segment_tree/practice3/C.cpp
+++ cpp/codeforces_itmo/segment_tree/practice3/C.cpp
@@ -6,12 +6,96 @@ using namespace std;
 
 class SegmentTree {
 
+    vector<int> tree;
+    vector<int> a;
+    int NEUTRAL = 0;
+    int n;
+
+    void build(int lx, int rx, int v) {
+        if (lx == rx) {
+            tree[v] = a[lx];
+            return;
+        }
+        int m = (lx + rx) / 2;
+        build(lx, m, 2 * v + 1);
+        build(m + 1, rx, 2 * v + 2);
+        tree[v] = tree[2 * v + 1] + tree[2 * v + 2];
+    }
+
+    void set(int i, int value, int lx, int rx, int v) {
+        if (lx == rx && lx == i) {
+            tree[v] = value;
+            return;
+        }
+        if (i < lx || rx < i) {
+            return;
+        }
+        int m = (lx + rx) / 2;
+        set(i, value, lx, m, 2 * v + 1);
+        set(i, value, m + 1, rx, 2 * v + 2);
+        tree[v] = tree[2 * v + 1] + tree[2 * v + 2];
+    }
+
+    int sum(int l, int r, int lx, int rx, int v) {
+        if (l <= lx && rx <= r) {
+            return tree[v];
+        }
+        if (rx < l || r < lx) {
+            return NEUTRAL;
+        }
+        int m = (lx + rx) / 2;
+        return sum(l, r, lx, m, 2 * v + 1) + sum(l, r, m + 1, rx, 2 * v + 2);
+    }
+
+public:
+
+    SegmentTree(vector<int> &a) {
+        n = pow(2, ceil(log2(a.size())));
+        this->a.resize(n);
+        for (int i = 0; i < a.size(); i++) {
+            this->a[i] = a[i];
+        }
+        tree.resize(2 * n - 1, NEUTRAL);
+        build(0, n - 1, 0);
+    }
+
+    void set(int i, int value) {
+        set(i, value, 0, n - 1, 0);
+    }
+
+    int sum(int l, int r) {
+        return sum(l, r, 0, n - 1, 0);
+    }
+
 };
 
 int main() {
 
     int n;
     cin >> n;
+    vector<int> numbers(2 * n);
+    vector<bool> visited(n + 1, false);
+    vector<int> indices(n + 1, 0);
+    vector<int> a(n, 0);
+    vector<int> answers(n + 1, 0);
+    SegmentTree tree = SegmentTree(a);
+
+
+
+    for (int i = 0; i < 2 * n; i++) {
+        cin >> numbers[i];
+        if (visited[numbers[i]]) {
+            tree.set(indices[numbers[i]], 1);
+            answers[numbers[i]] = tree.sum(indices[numbers[i]] + 1, i);
+        } else {
+            visited[numbers[i]] = true;
+            indices[numbers[i]] = i;
+        }
+    }
+
+    for (int i = 1; i < n + 1; i++) {
+        cout << answers[i] << " ";
+    }
 
     return 0;
 }
